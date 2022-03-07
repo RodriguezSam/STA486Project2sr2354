affbaffc2631f35e0dde85255bde87133bf364ff
sr2354 <54999900+sr2354@users.noreply.github.com>
2022-03-06 22:51:14 -0700

    query pull

--- docs/hospital-data.py
+++ docs/hospital-data.py
@@ -16,29 +16,30 @@ driver.switch_to.frame("iframe1")
 rows = 1 + len(driver.find_elements(by=By.XPATH, value="html/body/div[@id='main-page']/div[@id='jimu-layout-manager']/div/div/div/div[3]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[@id='ValuesTable']/div[@id='insertedTable_wrapper']/table[@id='insertedTable']/tbody[1]/tr"))
 cols = len(driver.find_elements(by=By.XPATH, value="html/body/div[@id='main-page']/div[@id='jimu-layout-manager']/div/div/div/div[3]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[@id='ValuesTable']/div[@id='insertedTable_wrapper']/table[@id='insertedTable']/thead[1]/tr[1]/th")) - 1
 headers = driver.find_elements(by=By.XPATH, value="html/body/div[@id='main-page']/div[@id='jimu-layout-manager']/div/div/div/div[3]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[@id='ValuesTable']/div[@id='insertedTable_wrapper']/table[@id='insertedTable']/thead[1]/tr[1]/th")
-for h in headers:
-    print(h.text)
-print(rows)
-print(cols)
+headers.pop(0)
+# for h in headers:
+#     print(h.text)
 
 df = pd.DataFrame(columns=[h.text for h in headers])
-df.head()
 
-for r in range(2,rows+1):
-    for c in range(1,cols+1):
-        value = driver.find_element(by=By.XPATH, value="html/body/div[@id='main-page']/div[@id='jimu-layout-manager']/div/div/div/div[3]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[@id='ValuesTable']/div[@id='insertedTable_wrapper']/table[@id='insertedTable']/tbody[1]/tr").text
+counties = driver.find_elements(by=By.XPATH, value="html/body/div[@id='main-page']/div[@id='jimu-layout-manager']/div/div/div/div[3]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[@id='ValuesTable']/div[@id='insertedTable_wrapper']/table[@id='insertedTable']/tbody[1]/tr")
+# for index, s in enumerate(counties):
+#     print('row {}:'.format(index))
+#     print('{}'.format(s.text))
 
-print(value, end="  ")
-print(len(value))
+col1 = driver.find_elements(by=By.XPATH, value="html/body/div[@id='main-page']/div[@id='jimu-layout-manager']/div/div/div/div[3]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[@id='ValuesTable']/div[@id='insertedTable_wrapper']/table[@id='insertedTable']/tbody/tr/td[2]")
+col2 = driver.find_elements(by=By.XPATH, value="html/body/div[@id='main-page']/div[@id='jimu-layout-manager']/div/div/div/div[3]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[@id='ValuesTable']/div[@id='insertedTable_wrapper']/table[@id='insertedTable']/tbody/tr/td[3]")
+col3 = driver.find_elements(by=By.XPATH, value="html/body/div[@id='main-page']/div[@id='jimu-layout-manager']/div/div/div/div[3]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[@id='ValuesTable']/div[@id='insertedTable_wrapper']/table[@id='insertedTable']/tbody/tr/td[4]")
 
-#driver.find_elements(by=By.ID, value="insertedTable")
+# print(col1, end='\n')
+
+df[df.columns[0]] = [s.text for s in col1]
+df[df.columns[1]] = [s.text for s in col2]
+df[df.columns[2]] = [s.text for s in col3]
+
+df.to_csv('hospital_data.csv')
+
 driver.quit()
-
-#soup = BeautifulSoup(page.content, "html.parser")
-
-#results = soup.find(id="iframe1")
-
-#print(results.prettify())
 
 
 
