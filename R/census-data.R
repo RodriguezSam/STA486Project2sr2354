8c7fe207be9d610ff6f26e61f7b22d25213048a5
sr2354 <54999900+sr2354@users.noreply.github.com>
2022-04-06 14:17:21 -0700

    analysis

--- R/census-data.R
+++ R/census-data.R
@@ -5,13 +5,95 @@
 #' Arizona as collected from 2010 to 2019 in American Community Surveys data
 #' profiles.
 #'
-#' @format A data frame with 8850 observations with 5 columns.
+#' @format A data frame with 100 observations with 86 columns.
 #' \describe{
-#'   \item{index}{numerical index for full dataframe}
-#'   \item{Labels}{the meaning to the value in the estimate column}
 #'   \item{Year}{the year of the given information}
 #'   \item{County}{the county of Arizona the value came from}
-#'   \item{Estimate}{the estimated population value for the given label}
+#'   \item{County.Year}{the combination of the county and year column}
+#'   \item{Total.Population}{the total population of the location}
+#'   \item{Male.Total.Population}{the total population of males}
+#'   \item{Female.Total.Population}{the total population of females}
+#'   \item{Sex.Ratio}{males per 100 females}
+#'   \item{Under.5.Population}{the total population of under 5 years old}
+#'   \item{5.to.9.Population}{the total population of 5 to 9 years of age}
+#'   \item{10.to.14.Population}{the total population of 10 to 14 years of age}
+#'   \item{15.to.19.Population}{the total population of 15 to 19 years of age}
+#'   \item{20.to.24.Population}{the total population of 20 to 24 years of age}
+#'   \item{25.to.34.Population}{the total population of 25 to 34 years of age}
+#'   \item{35.to.44.Population}{the total population of 35 to 44 years of age}
+#'   \item{45.to.54.Population}{the total population of 45 to 54 years of age}
+#'   \item{55.to.59.Population}{the total population of 55 to 59 years of age}
+#'   \item{60.to.64.Population}{the total population of 60 to 64 years of age}
+#'   \item{65.to.74.Population}{the total population of 65 to 74 years of age}
+#'   \item{75.to.84.Population}{the total population of 75 to 84 years of age}
+#'   \item{Over.85.Population}{the total population of over 85 years old}
+#'   \item{Median.Age}{the median age for the county at the year}
+#'   \item{Under.18.Population}{the total population under 18 years of age}
+#'   \item{Over.16.Population}{the total population of over 16 years of age}
+#'   \item{Over.18.Population}{the total population of over 18 years of age}
+#'   \item{Over.21.Population}{the total population of over 21 years of age}
+#'   \item{Over.62.Population}{the total population of over 62 years of age}
+#'   \item{Over.65.Population}{the total population of over 65 years of age}
+#'   \item{Over.18}{the total population over 18 years of age}
+#'   \item{Over.18.Male}{the total population males over 18 years of age}
+#'   \item{Over.18.Female}{the total population females over 18 years of age}
+#'   \item{Over.18.Sex.Ratio}{males per 100 females over 18 years of age}
+#'   \item{Over.65.Male}{the total population of males over 65 years of age}
+#'   \item{Over.65.Female}{the total population of females over 65 years of age}
+#'   \item{Over.65.Sex.Ration}{males per 100 females over 65 years of age}
+#'   \item{Male.Total.Population}{the total population of males}
+#'   \item{One.Race.Total.Population}{the total population of people under one race}
+#'   \item{Two.Or.More.Races.Population}{the total population of people under two or more races}
+#'   \item{White}{the total population of white people}
+#'   \item{Black.Or.African.American}{the total population of black or african american people}
+#'   \item{AI.and.AN}{the total population of american indian or alaskan native people}
+#'   \item{Cherokee.TG}{the total population of Cherokee tribal grouping}
+#'   \item{Chippewa.TG}{the total population of Chippewa tribal grouping}
+#'   \item{Navajo.TG}{the total population of Navajo tribal grouping}
+#'   \item{Sioux.TG}{the total population of Sioux tribal grouping}
+#'   \item{Asian}{the total population of Asian people}
+#'   \item{Asian.Indian}{the total population of Asian Indian people}
+#'   \item{Chinese}{the total population of Chinese people}
+#'   \item{Filipino}{the total population of Filipino people}
+#'   \item{Japanese}{the total population of Japanese people}
+#'   \item{Korean}{the total population of Korean people}
+#'   \item{Vietnamese}{the total population of Vietnamese people}
+#'   \item{Other.Asian}{the total population of people other than Asian}
+#'   \item{NH.and.OPI}{the total population of Native Hawaiian and Other Pacific Islander people}
+#'   \item{NH}{the total population of Native Hawaiians}
+#'   \item{Guamanian.or.Chamorro}{the total population of Guamanian or Chamorro people}
+#'   \item{Samoan}{the total population of Samoan people}
+#'   \item{OPI}{the total population of other Pacific Islander people}
+#'   \item{Some.Other.Race}{the total population of some other race}
+#'   \item{White.and.Black.or.AA}{the total population of White and Black or African American people}
+#'   \item{White.and.AI.and.AN}{the total population of White and American Indian and Alaskan Native people}
+#'   \item{White.and.Asian}{the total population of White and Asian people}
+#'   \item{Black.or.AA.and.AI.and.AN}{the total population of Black or African American and American Indian and Alaskan Native people}
+#'   \item{Race.Alone.White}{the total population of all White people}
+#'   \item{Race.Alone.Black.or.AA}{the total population of all Black or African American people}
+#'   \item{Race.Alone.AI.and.AN}{the total population of all American Indian and Alaskan Native people}
+#'   \item{Race.Alone.Asian}{the total population of all Asian people}
+#'   \item{Race.Alone.NH.and.OPI}{the total population of all Native Hawaiians and Other Pacific Islander people}
+#'   \item{Race.Alone.Other}{the total population of all other races}
+#'   \item{HL}{the total population of Hispanic or Latino people}
+#'   \item{Mexican}{the total population of Mexican people}
+#'   \item{Puerto.Rico}{the total population of people Puerto Rican people}
+#'   \item{Cuban}{the total population of Cuban people}
+#'   \item{OH.or.LP}{the total population of other Hispanic or Latino people}
+#'   \item{Not.Hispanic.or.Latino}{the total population of people that not Hispanic or Latino}
+#'   \item{NHL.White}{the total population of Non-Hispanic White people}
+#'   \item{NHL.Black.or.AA}{the total population of Non-Hispanic Black or African American people}
+#'   \item{NHL.AI.and.AN}{the total population of Non-Hispanic American Indian and Alaskan Native people}
+#'   \item{NHL.Asian.Alone}{the total population of Non-Hispanic Asian people}
+#'   \item{NHL.NH.and.OPI}{the total population of Non-Hispanic Native Hawaiian and Other Pacific Islander people}
+#'   \item{NHL.Other}{the total population of Non-Hispanic Other Races}
+#'   \item{NHL.Two.or.More.Races}{the total population of Non-Hispanic of two or more races}
+#'   \item{NHL.Two.Races.Including}{the total population of Non-Hispanic of two or more races including other races}
+#'   \item{NHL.Two.Races.Excluding}{the total population of Non-Hispanic of two or more races excluding other races, and three or more races}
+#'   \item{HL.Total.Housing.Units}{total housing units of Hispanic people}
+#'   \item{Citizens.Over.18}{the total population of citizens over 18 years of age}
+#'   \item{Male.Citizens.Over.18}{the total population of male citizens over 18 years of age}
+#'   \item{Female.Citizens.Over.18}{the total population of female citizens over 18 years of age}
 #' }
 #' @source \url{https://data.census.gov/cedsci/table}
 "censusData"
@@ -188,8 +270,17 @@ censusData <- census[!(census$Labels == "SEX.AND.AGE" |
 censusData$Labels <- as.factor(censusData$Labels)
 censusData$Year <- as.character(censusData$Year)
 censusData$Year <- as.Date(censusData$Year, "%Y")
+censusData$County <- tolower(censusData$County)
 censusData$County <- as.factor(censusData$County)
 censusData$Estimate <- as.numeric(gsub(",","",censusData$Estimate))
+censusData$County.Year <- paste(censusData$County, censusData$Year)
+censusData <- censusData %>% select(-index)
+censusData <- pivot_wider(censusData, names_from = Labels, values_from = Estimate)
+censusData <- censusData %>% select(-Over.65) %>%
+  select(-Race.Total.Population) %>% select(-One.Race) %>%
+  select(-Two.Or.More.Races) %>% select(-Race.Alone.Population) %>%
+  select(-HL.Total.Population)
+
 
 
 usethis::use_data(censusData, overwrite=TRUE)
--- R/hospital-data.R
+++ R/hospital-data.R
@@ -4,15 +4,14 @@
 #' being measured. There are 9 illness being measured over the different Arizona
 #' counties that have data. The years are from 2005 to 2019.
 #'
-#' @format A data frame with 1535 observations with 7 columns.
+#' @format A data frame with 874 observations with 6 columns.
 #' \describe{
-#'   \item{index}{numerical index for full dataframe}
 #'   \item{County}{the county the data pertains to}
 #'   \item{County.Value}{the value per 10,000 population affected by the specific illness in the county}
 #'   \item{State.Rate}{the value per 10,000 population of the state of Arizona}
 #'   \item{Year}{the year of the given information}
 #'   \item{Content.Area}{the given illness}
-#'   \item{State.Avg}{the value per 10,000 population of the state of Arizona}
+#'   \item{County.Year}{combination of the county and year}
 #' }
 #' @source \url{https://gis.azdhs.gov/ephtexplorer/}
 "hospitalData"
@@ -23,6 +22,7 @@
 library(usethis)
 
 hospitalData <- read.csv('docs/hospital_data.csv')
+hospitalData$County <- tolower(hospitalData$County)
 hospitalData$County <- as.factor(hospitalData$County)
 hospitalData$Value <- as.integer(hospitalData$Value)
 hospitalData$Year <- as.character(hospitalData$Year)
@@ -32,9 +32,20 @@ hospitalData$State..Avg. <- as.integer(hospitalData$State..Avg.)
 colnames(hospitalData) <- c("index", "County", "County.Value", 'State.Rate',
                             "Year", "Content.Area", "State.Avg")
 hospitalData <- hospitalData[!(hospitalData$County == "GILA" |
-                         hospitalData$County == "GRAHAM" |
-                         hospitalData$County == "GREENLEE" |
-                         hospitalData$County == "LA PAZ" |
-                           hospitalData$County == "SANTA CRUZ"),]
+                         hospitalData$County == "graham" |
+                         hospitalData$County == "greenlee" |
+                         hospitalData$County == "la paz" |
+                           hospitalData$County == "santa cruz"),]
+hospitalData <- hospitalData[!(hospitalData$Year == "2005-04-05" |
+                                 hospitalData$Year == "2006-04-05" |
+                                 hospitalData$Year == "2007-04-05" |
+                                 hospitalData$Year == "2008-04-05" |
+                                 hospitalData$Year == "2009-04-05"),]
+hospitalData$County.Year <- paste(hospitalData$County, hospitalData$Year)
+hospitalData <- hospitalData %>% select(-index)
+hospitalData <- hospitalData %>%
+  mutate(State.Rate=ifelse(is.na(State.Rate), State.Avg, State.Rate)) %>%
+  select(-State.Avg)
+
 
 usethis::use_data(hospitalData, overwrite=TRUE)
